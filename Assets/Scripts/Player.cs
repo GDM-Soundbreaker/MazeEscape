e6ebf6b8cea87cd9572a476e2964b8a13a9ad989
GDM-Soundbreaker <43067364+GDM-Soundbreaker@users.noreply.github.com>
2018-09-28 16:21:28 +0100

    Added character and movement code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //designer variables
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxi = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get axis input from unity
+        float leftRight = Input.GetAxis(horizontalAxi);
+        float upDown = Input.GetAxis(verticalAxis);
+
+        //create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, upDown);
+
+        //make direction vector length 1
+        direction = direction.normalized;
+
+        //calculate velocity
+        Vector2 velocity = direction * speed;
+
+        //give the velocity to the rigidbody
+        physicsBody.velocity = velocity;
+	}
+}
