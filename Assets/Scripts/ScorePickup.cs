7c5316866f2468fdf625da3f245f5b4e0e8ae1d1
GDM-Soundbreaker <43067364+GDM-Soundbreaker@users.noreply.github.com>
2018-10-05 14:53:10 +0100

    Added tilemap, collision and camera tracking

---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ///only if player hits
+        if (collision.GetComponent <Player>())
+        {
+            //updating the score based on how much item is worth
+            scoreTracker.ChangeValue(scoreValue);
+
+            //get rid of used item
+            Destroy(gameObject);
+        }
+    }
+
+}
